9098d579fba42469566c10adb9621794f8d3275f
Karol W <karol.wichmann@Outlook.com>
2020-03-10 22:58:37 +0100

    add fields for image and title.TODO:Style+Content

--- src/components/Uslugi.js
+++ src/components/Uslugi.js
@@ -1,9 +1,35 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import UslugiPhoto from "./../media/uslugi.png";
 
 const Uslugi = () => {
-
   return (
-    <div>Uslugi</div>
+    <Container>
+      <Row>
+        <Col sm={3}>
+          <Paper>
+            <h5>
+              mgr inż. ADAM PONIATOWSKI
+              <br />
+              kierownik budowy, projektant, <br />
+              inspektor nadzoru inwestorskiego
+              <br />
+              uprawnienia bez ograniczeń w branży konstrukcyjno-budowlanej
+            </h5>
+          </Paper>
+        </Col>
+        <Col sm={9}>
+          <img
+            className={"uslugi-photo"}
+            src={UslugiPhoto}
+            alt={"uslugi"}
+          ></img>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
