d9a94e8006c6336e1b2cd00e780c978e62c4b2e4
Karol W <karol.wichmann@Outlook.com>
2020-04-05 19:50:56 +0200

    initial build for go-live

---
+++ precache-manifest.e6cb13d82fb4401e7c1d3fcafa156d48.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1117c25d22a9da2bbd52b7f2e7f2adb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc43e084ef0892d34c1d",
+    "url": "/static/css/main.60270609.chunk.css"
+  },
+  {
+    "revision": "de1470eb2e2c20e531c3",
+    "url": "/static/js/2.85503568.chunk.js"
+  },
+  {
+    "revision": "d7b319d8a6f4d5773bf6cc977c7731d1",
+    "url": "/static/js/2.85503568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc43e084ef0892d34c1d",
+    "url": "/static/js/main.d4101f59.chunk.js"
+  },
+  {
+    "revision": "bcf8faf44aba12d515c5",
+    "url": "/static/js/runtime-main.ce5fb8d3.js"
+  },
+  {
+    "revision": "18f92c5811c06fcf7b53c272a41dc078",
+    "url": "/static/media/april.18f92c58.png"
+  },
+  {
+    "revision": "11e19a3953e25bdaa56940f66e3bf6ce",
+    "url": "/static/media/august.11e19a39.png"
+  },
+  {
+    "revision": "a8c7e029c2791df47186176c3b506462",
+    "url": "/static/media/banner.a8c7e029.png"
+  },
+  {
+    "revision": "11e19a3953e25bdaa56940f66e3bf6ce",
+    "url": "/static/media/july.11e19a39.png"
+  },
+  {
+    "revision": "11e19a3953e25bdaa56940f66e3bf6ce",
+    "url": "/static/media/june.11e19a39.png"
+  },
+  {
+    "revision": "11e19a3953e25bdaa56940f66e3bf6ce",
+    "url": "/static/media/march.11e19a39.png"
+  },
+  {
+    "revision": "c5302e6858adcdbb26f1b870d9c20843",
+    "url": "/static/media/may.c5302e68.png"
+  },
+  {
+    "revision": "11e19a3953e25bdaa56940f66e3bf6ce",
+    "url": "/static/media/september.11e19a39.png"
+  },
+  {
+    "revision": "66902eff070cbbad2aea82df6dfc9377",
+    "url": "/static/media/uslugi.66902eff.png"
+  }
+]);
--- src/components/Online.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Online = () => {
-  return <div className="container pages">Online</div>;
-};
-
-export default Online;
